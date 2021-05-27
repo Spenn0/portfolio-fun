9af6451b77c902bfa22b73630b07040558fdac38
Spenn0 <sabcompositions@gmail.com>
2021-05-27 14:53:10 -0400

    update

--- script.js
+++ script.js
@@ -31,3 +31,46 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   // dots[slideIndex - 1].className += " active";
 }
+
+// for the second carousel. I know there's a better way to do this, don't judge me.
+//
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides2.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides2.length;
+  }
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+  console.log("hey");
+}
+
+function toggleDarkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+// document.
